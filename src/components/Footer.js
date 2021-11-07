4a25355b348f795c29510bb8c1cad54492b2ae34
FelAnji <oniyeleanjola@gmail.com>
2021-11-07 23:14:38 +0100

    this is the footer section of google homepage

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className='country'>Nigeria</div>
+            <div className='section2'>
+                <div className='carbon'><a href='#'>Carbon neutral since 2007</a></div>
+                    <div className='nav-links'>
+                        <ul>
+                            <li><a href='#'>About</a></li>
+                            <li><a href='#'>Advertising</a></li>
+                            <li><a href='#'>Business</a></li>
+                            <li><a href='#'>How Search works</a></li>
+                        </ul>
+                    </div>
+                    <div className='footer2'>
+                        <ul>
+                            <li><a href='#'>Privacy</a></li>
+                            <li><a href='#'>Terms</a></li>
+                            <li><a href='#'>Settings</a></li>
+                        </ul>
+                    </div>  
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
