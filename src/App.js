8f4e2e2e621e0a97bd8f6c69f8e7a08b1a6741bb
Timibreez <ffftimi@gmail.com>
2021-11-08 02:42:26 +0100

    fixed footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Homepage from './components/homepage';
 import Navigation from './components/navigation';
+import Footer from './components/footer'
 
 function App() {
   return (
     <div>
       <Navigation />
       <Homepage />
+      <Footer />
     </div>
     
   );
